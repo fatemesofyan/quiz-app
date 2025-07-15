f8e790569078566ae89189e0cf693ceded5c3f4b
Fateme Sofyan <fatemesofyan1576@gmail.com>
2025-07-15 12:01:29 +0330

    fix: remove unused ReactNode import in Context.tsx and add ReactNode import in types.ts

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,17 +1,14 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, useState } from "react";
 import { IProps, IQuiz } from "../interface/types";
 
 export const Quizecontext = createContext<any>(null);
 
+export const QuizProvider = ({ children }: IProps) => {
+  const [quizelist, setQuizelist] = useState<IQuiz[]>([]);
 
-export const QuizProvider = ({children}:IProps) => {
-  
-     const [quizelist, setQuizelist] = useState<IQuiz[]>([]);
-
-    return (
-      <Quizecontext.Provider value={{ quizelist, setQuizelist}}>
-        {children}
-      </Quizecontext.Provider>
-    );
-  };
-  
+  return (
+    <Quizecontext.Provider value={{ quizelist, setQuizelist }}>
+      {children}
+    </Quizecontext.Provider>
+  );
+};
--- src/interface/types.ts
+++ src/interface/types.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 export interface IQustion {
   count: string | number;
   category: string;
